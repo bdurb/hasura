d03cf38118244fb53735f58becd0ebdc15e02209
bdurb <brian@rpmgreenville.com>
2019-01-31 08:53:09 -0800

    set up quotelist component

---
+++ src/components/QuoteList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2 v-if="loading">Loading...</h2>
+    <quote
+      v-for="quote in quotes"
+      :key="quote.id"
+      :quote="quote">
+    </quote>
+  </div>
+</template>
+
+
+<script>
+import { ALL_QUOTES_QUERY } from '../query-helpers/graphql.js'
+import Quote from './Quote'
+
+export default {
+  name: 'QuoteList',
+  data() {
+    return {
+      quotes: [],
+      loading: 0
+    }
+  },
+  components: {
+    Quote
+  },
+  apollo: {
+    quotes: {
+      query: ALL_QUOTES_QUERY
+    }
+  }
+}
+</script>
