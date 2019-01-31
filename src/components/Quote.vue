43966411f1267cbb3a79f2e0e7bd65c471af7640
bdurb <brian@rpmgreenville.com>
2019-01-31 08:45:50 -0800

    set up quote component to display quote and author

---
+++ src/components/Quote.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>{{quote.quote}}</h1>
+    <h3>by {{quote.author}} </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Quote',
+  props: ['quote']
+}
+</script>
+
